fee86c8fc7f28aef9ef0e88a362e49d9a14c8d32
ronaktyagi123 <76524934+ronaktyagi123@users.noreply.github.com>
2021-05-25 06:21:37 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,100 @@
+function preload() {
+    appleimg = loadImage("apple.png")
+    blueghostimg = loadImage("blue ghost.png")
+    redghostimg = loadImage("red ghost.png")
+    pinkghostimg = loadImage("Pink ghost.png")
+    backgroundimg = loadImage("bg 1.jpg")
+    backgroundimg = loadImage("bg 2.jpg")
+    cherryimg = loadImage("cherry.png")
+    pacmanimg = loadImage("pac man.gif")
+    strawberryimg = loadImage("strawberry.png")
+    wallimg = loadImage("wall.png")
+    yellowghostimg = loadImage("yellow ghost.png")
+}
+
+
+
+
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    pacman = createSprite(50, height - 200, 50, 50)
+    pacman.scale = 0.3
+    pacman.addImage(pacmanimg)
+    for (var i = 0; i < width * 5; i += 80) {
+        ground = createSprite(i, height - 100, width, 20)
+        ground.addImage(wallimg)
+        ground.scale = 0.5
+    }
+}
+function draw() {
+    background(backgroundimg)
+    spawnObstacles()
+    spawnFruits()
+    drawSprites()
+}
+
+function spawnObstacles() {
+    if (frameCount % 250 === 0) {
+        var obstacle = createSprite(width, height-120 , 10, 40);
+        obstacle.velocityX = -6;
+
+
+        // //generate random obstacles
+        var rand = Math.round(random(1, 4));
+        switch (rand) {
+            case 1: obstacle.addImage(yellowghostimg);
+            obstacle.scale=0.05
+                break;
+            case 2: obstacle.addImage(pinkghostimg);
+            obstacle.scale=0.05
+                break;
+                
+            case 3: obstacle.addImage(redghostimg);
+            obstacle.scale=0.05
+                break;
+                
+            case 4: obstacle.addImage(blueghostimg);
+            obstacle.scale=0.2
+                break;
+           
+            default: break;
+        }
+        obstacle.y = Math.round(random(height-500,height-180));
+        //assign scale and lifetime to the obstacle           
+        
+        obstacle.lifetime = 500;
+    }
+}
+
+function spawnFruits() {
+    if (frameCount % 150 === 0) {
+        var obstacle = createSprite(width, height-120 , 10, 40);
+        obstacle.velocityX = -6;
+
+
+        // //generate random obstacles
+        var rand = Math.round(random(1, 3));
+        switch (rand) {
+            case 1: obstacle.addImage(appleimg);
+            obstacle.scale=0.105
+                break;
+            case 2: obstacle.addImage(cherryimg);
+            obstacle.scale=0.02
+                break;
+                
+            case 3: obstacle.addImage(strawberryimg);
+            obstacle.scale=0.105
+                break;
+                
+            
+           
+            default: break;
+        }
+        obstacle.y = Math.round(random(height-500,height-180));
+        //assign scale and lifetime to the obstacle           
+        
+        obstacle.lifetime = 500;
+    }
+}
+
+
